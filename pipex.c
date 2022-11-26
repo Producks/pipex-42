6fd97c28b50ca76b502f6b15b151bd39bad5aed5
Dave Demers <56704919+Producks@users.noreply.github.com>
2022-11-26 06:39:45 -0500

    Add files via upload

--- pipex.c
+++ pipex.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
 
 int main(void)
 {
-    printf("hello");
-    return (0);
+    int fd;
+
+    fd = open("lol", "r");
+    printf("%s", strerror(69));
+    exit(-1);
 }
